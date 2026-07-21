5f69d93f2a96282baefb58398f72497aee55a3ef
Gelo Reyes <reyesgelo00@gmail.com>
2026-07-22 02:26:45 +0800

    Add: Added set operations

--- 03.sets.py
+++ 03.sets.py
@@ -87,7 +87,7 @@ print (fruitCopy)
 # SET OPERATIONS
     # UNION - Combine a set and returns the unique values
     # DIFFERENCE - lahat ng meron si setOne na meron si setTwo ay matatanggal
-    # SYMMETRIC DIFFERENCE - lahat ng walang kaparehas na value sa mga set, ay rereturn ng system
+    # SYMMETRIC DIFFERENCE - lahat ng walang kaparehas na value sa mga set, ay rereturn ng system. 2 sets lang ang pwede icompare using this.
 
 unangSet = {"Karen", "Gelo", "Ikang"}
 secondSet = {"Karen", "Wyne"}
@@ -104,11 +104,49 @@ print (fourthSet)
 fifthSet = secondSet.difference (unangSet)
 print (fifthSet)
 
-#Symmetric Difference - irereturn yung mga values na walang kaparehas, di imporntant yung order dito sa kung ano ang mauunang set dahil iipunin naman ng system yung walang mga kagaya na values
+#Symmetric Difference - irereturn yung mga values na walang kaparehas, di imporntant yung order dito sa kung ano ang mauunang set dahil iipunin naman ng system yung walang mga kagaya na values. 2 sets lang ang pwede icompare using this.
 sample_set_1 = {1,2,3,4,5,6,7,8,9,10}
 sample_set_2 = {4,5,6,7}
 sample_set_3 = {8,9,10,11,12,13,14,15}
 
-sample_set_4 = sample_set_1.symmetric_difference (sample_set_2, sample_set_3)
+sample_set_4 = sample_set_1.symmetric_difference (sample_set_2)
 print (sample_set_4)
 #Output: sample_set_4 = {1,2,3,11,12,13,14,15}
+
+
+#Disjoint Set
+# a function na nagchecheck kung may magkaparehas na value/item ang dalawang set.
+# nagrereturn ng TRUE or FALSE. True kung may magkaparehas and FALSE kung wala.
+# Hindi mahalaga ang ordering neto dahil both set ang cinocompare
+print(sample_set_1.isdisjoint(sample_set_2))
+# Output: FALSE
+
+#Subset
+# Yung set one tinitignan kung siya ay nasa set two.
+# Small inside Big: ll the small box items are inside the big box.
+numbers = {1,2,3,4,5,6,7,8,9,10}
+even_Numbers = {2,4,6,8,10}
+
+print(evenNumbers.issubset(numbers))
+#Output: TRUE - dahil lahat ng numbers na 2,4,6,8,10 ay nageexist sa numbers set.
+#Now if ang isang number mula sa even number ay hindi existing sa numbers, magfafalse na ito kaagad.
+
+#Superset
+#Big holds Small - the big box has all the small box items (and maybe more).
+print(numbers.issuperset(even_Numbers))
+#Output: TRUE - dahil yung numbers (superset) ay may value na kagaya sa even_Numbers (subset)
+#Now if ang isang number mula sa even number ay hindi existing sa numbers, magfafalse na ito kaagad.
+
+#Casting Set
+
+#Set to List
+numbers = list(numbers)
+print ("This is a set, ", numbers)
+
+#List to Tuples
+numbers = tuple(numbers)
+print ("This is a set, ", numbers)
+
+#Tuples to Set
+numbers = set(numbers)
+print ("This is a set, ", numbers)
