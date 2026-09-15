c4b44f7d6eefdebd9021ba3cfd9f809d052b4d68
Gelo Reyes <reyesgelo00@gmail.com>
2026-09-15 20:05:13 +0800

    Completed

--- 31. Convenience Store Checkout App.py
+++ 31. Convenience Store Checkout App.py
@@ -34,8 +34,8 @@ while True:
 
         if user_choice == 1:
             print ("Please see inventory items below:")
-            for everyproduct in products:
-                print("- " + everyproduct)
+            for everyproduct in range(len(products)):
+                print("- ", products[everyproduct], prices[everyproduct])
             print ("All inventory items are printed")
 
         elif user_choice == 2:
@@ -89,16 +89,64 @@ while True:
 
 
         elif user_choice == 3:
-            print (3)
+            if len(cart) > 0:
+                print ("Your Cart: ")
+                for every_item in cart:
+                    print (every_item)
+                print ("--- Nothing Follows ---")
+
+            else:
+                print ("--- Nothing is in your cart now. Order now ---")
+                print ("Returning back to the menu ...")
 
         elif user_choice == 4:
-            print (4)
+            print ("Entering to checkout ...")
+            if len(cart) > 0:
+                print ("Your Cart: ")
+                for every_item in range (len(cart)):
+                    print (cart[every_item], " - ", cart_prices[every_item])
+                print ("Total is: ", sum(cart_prices))
+                print ("--- Nothing Follows ---")
+
+                checkout_confirmation = str(input("Confirm checkout? Type YES[Y] or NO[N] :"))
+                if checkout_confirmation.upper() == "Y":
+                    amountdue = sum(cart_prices)
+                    print ("Your total amount due is ", amountdue)
+
+                    paymentconfirmation = str(input("Payment Successful? Type YES[Y] or NO[N] :"))
+                    if paymentconfirmation.upper() == "Y":
+                        print ("Payment Sucessful")
+                        cart.clear()
+                        cart_prices.clear()
+
+                    elif paymentconfirmation.upper() == "N":
+                        print ("Exiting the system ...")
+                        print ("Thank you!")
+                        break
+
+                    else:
+                        print ("Returning back to the menu ...")
+                        continue
+
+                elif checkout_confirmation.upper() == "N":
+                    print ("Exiting the system ...")
+                    print ("Thank you!")
+                    break
+
+                else:
+                    print ("Returning back to the menu ...")
+                    continue
+
+            else:
+                print ("--- Nothing is in your cart now. \nNothing to checkout. \nOrder now ---")
+                print ("Returning back to the menu ...")
 
         elif user_choice == 5:
-            print (5)
+            print ("Exiting the system ...")
+            break
 
         else:
-            print (user_choice + " is not in the options")
+            print (user_choice, " is not in the options")
 
     except ValueError:
         print ("You have entered an invalid input")
@@ -110,7 +158,7 @@ while True:
             continue
 
         elif wanttocontinue.upper() == "N":
-            print ("Exiting the system")
+            print ("Exiting the system ...")
             print ("Thank you!")
             break
 
